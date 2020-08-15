6e078000fc7c63d290e7930a992a237bfff7d624
MasonWang025 <mason.wang0025@gmail.com>
2020-08-15 14:48:07 -0700

    reciever.py

---
+++ reciever.py
@@ -0,0 +1,54 @@
+import email
+import imaplib
+
+# connect to the server and go to its inbox
+server = imaplib.IMAP4_SSL('imap.gmail.com')  # port 993
+
+# login
+with open('password.txt', 'r') as f:
+    password = f.read()
+server.login("mail.testing0025@gmail.com", password)
+server.select("INBOX")
+
+# search using ALL criteria, get all unread messages
+# (data is list of uids), use 'ALL' if want all emails
+status, data = server.search(None, '(UNSEEN)')
+
+# extract mail_ids
+mail_ids = []
+for block in data:
+    mail_ids += block.split()  # b'1 2 3'.split() => [b'1', b'2', b'3']
+
+# extract each email and contents
+for i in mail_ids:
+    # the fetch function fetch the email given its id and format
+    status, data = server.fetch(i, '(RFC822)')
+
+    # '(RFC822)' => a list with a tuple with header, content, and the closing byte b')'
+    for response_part in data:
+        # so if its a tuple...
+        if isinstance(response_part, tuple):
+            # we go for the content at its second element
+            # skipping the header at the first and the closing
+            # at the third
+            message = email.message_from_bytes(response_part[1])
+
+            mail_from = message['from']
+            mail_subject = message['subject']
+
+            # if not all plain text, seperate from annexes
+            if message.is_multipart():
+                mail_content = ''
+                # if multipart, loop through payload
+                for part in message.get_payload():
+                    # if the content type is text/plain we extract it
+                    if part.get_content_type() == 'text/plain' or part.get_content_type() == 'text/html':
+                        mail_content += part.get_payload()
+            else:
+                # if the message isn't multipart, just extract it
+                mail_content = message.get_payload()
+
+            # and then let's show its result
+            print(f'From: {mail_from}')
+            print(f'Subject: {mail_subject}')
+            print(f'Content: {mail_content}')
--- sender.py
+++ sender.py
@@ -4,7 +4,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 
-# gmail server
+# connect to server
 server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
 
